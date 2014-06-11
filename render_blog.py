1ccb8933f027a1a4f59b3000e7596754d574068c
zancas <zancas@leastauthority.com>
2014-06-10 19:38:04 -0600

    cp css so it can be rendered

--- render_blog.py
+++ render_blog.py
@@ -4,6 +4,7 @@ def render_blog():
     # copy _base and _navlinks from main templates directory
     subprocess.check_call(['scp', '../leastauthority.com/lae_site/templates/_base.html', 'themes/lae/templates/_base.html'])
     subprocess.check_call(['scp', '../leastauthority.com/lae_site/templates/_navlinks.html', 'themes/lae/templates/_navlinks.html'])
+    subprocess.check_call(['scp', '../leastauthority.com/content/static/css/style.css', 'themes/lae/static/style.css'])
 
     # run pelican
     subprocess.check_call(['pelican', '-v', '-s', 'conf.py'])
